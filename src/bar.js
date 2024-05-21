4e11d4a0bfc299e6a16e75ef2e99fd130678833a
w2xi <43wangxi@gmail.com>
2024-05-21 18:55:22 +0800

    chore: a bundler demo and support json file

---
+++ src/bar.js
@@ -0,0 +1,9 @@
+import info from './test.json'
+
+console.log('bar.js')
+
+console.log('json info:', info)
+
+export default {
+    name: 'bar'
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import createHeader from './header.js';
+import name from './bar.js'
 
 const header = createHeader();
 
 document.body.append(header);
+
+console.log('bar name', name);
