9eba455580554b467c821ddea1798a6c4a7e1e02
w2xi <43wangxi@gmail.com>
2024-05-21 08:26:32 +0800

    chore: init

---
+++ src/header.js
@@ -0,0 +1,5 @@
+export default function createHeader() {
+  const header = document.createElement('h1');
+  header.innerText = 'Hello World';
+  return header;
+}
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import createHeader from './header.js';
+
+const header = createHeader();
+
+document.body.append(header);
